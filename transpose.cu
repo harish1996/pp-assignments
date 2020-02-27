8981dd1773440e5b0dfcb1330a259a37fcf622df
Harish Ganesan <mtc1818@isical.ac.in>
2020-02-27 23:23:18 +0530

    Symmetricity checking: Initial unnecessarily complicated version

---
+++ transpose.cu
@@ -0,0 +1,268 @@
+/************************************************
+ * MATRIX TRANSPOSE CHECK between parallel
+ * 	and sequential programs.
+ *
+ * Usage:
+ * 	Compile using nvcc -lcudart transpose.cu -o transpose
+ *	Run using ./mat <size of the matrix>
+ *
+ * Example:
+ *	./mat 153
+ *	The above will check whether for a random matrix, A = transpose(A)
+ *
+ ************************************************/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CHECK_STATUS(status)	if( status != cudaSuccess ){ fprintf(stderr,"Error at %d\n",__LINE__); return -1; }
+
+void generate_assymmetric_matrix( float *A, int size, float assymmetricity )
+{
+	float r;
+	/* Initializing random number generator */
+	srand( time(NULL) );
+	
+	for( int i=0; i<size; i++ ){
+		for( int j=0; j<=i; j++ ){
+			A[i*size + j] = (float)rand() / 100000;
+			r = (float)( (rand()%30000) * 10 ) / 30000 ;
+		      	if ( r < assymmetricity ){
+				A[ j*size + i ] = (float)rand() / 100000;
+				//printf("%f %d %d\n",r,i,j);
+			}
+			else	
+				A[j*size + i] = A[i*size + j];
+		}
+	}
+}
+
+void generate_symmetric_matrix( float *A, int size )
+{
+	/* Initializing random number generator */
+	srand( time(NULL) );
+	
+	for( int i=0; i<size; i++ ){
+		for( int j=0; j<=i; j++ ){
+			A[i*size + j] = rand() / 100000;
+			A[j*size + i] = A[i*size + j];
+		}
+	}
+}
+	
+void generate_notso_random_matrix( float *A, int size )
+{
+	srand( time(NULL) );
+
+	int r = rand()%100;
+	if( r <= 50 )
+		generate_assymmetric_matrix( A, size, 0.1 );
+	else
+		generate_symmetric_matrix( A, size );
+}
+
+/**
+ * Parallel routine for checking whether the matrix is symmetric
+ * Divides the work of checking symmetricity by allocating processors for each element in
+ * in the bottom triangle of the matrix.
+ */
+__global__ void par_is_symmetric( float* A, bool* is_transpose, int size )
+{
+	int c = blockIdx.x * blockDim.x + threadIdx.x;
+	int r = blockIdx.y * blockDim.y + threadIdx.y;
+	int actual_row,actual_column;
+	int max_row = size / 2 + ( size % 2 == 0?0:1 );
+
+	if( !is_transpose ){
+		return;
+	}
+
+	if( (r < max_row) || (r == max_row && size%2 == 0) ){
+		if( c < r ){
+			actual_row = r;
+		       	actual_column = c;
+		}
+		else if(c < size-1){
+			actual_row = size -1- r;
+			actual_column = c - r;
+		}
+		else{
+			return;
+		}
+	}
+	else if( r == max_row ){
+		if( c < r ){
+			actual_row = r;
+			actual_column = c;
+		}
+		else
+			return;
+	}
+	else{
+		return;
+	}
+	
+	if( A[ actual_row*size + actual_column ] != A[ actual_column*size + actual_row ] )
+		*is_transpose = false;
+}
+
+
+/**
+ * Sequential matrix symmetricity checking function
+ */
+void seq_is_symmetric( float *A, bool* is_transpose, int size )
+{
+	*is_transpose = true;
+	for( int i=0; i<size; i++ ){
+		for( int j=0; j<i; j++ ){
+			if( A[i*size + j] != A[j*size + i] ){
+			       *is_transpose = false;
+			       return;
+			}
+		}
+	}
+}
+
+
+int main( int argc, char* argv[] )
+{
+	/* Matrix container pointers */
+	float *A;
+	float *ga;
+
+	int size,size_x;	/* Size of the matrix */
+	int matrix_size;	/* Physical size of the matrix in the memory */
+	
+	int num_blocks_x,num_blocks_y;		
+	
+	cudaEvent_t start,stop;
+	
+	bool do_print=false;	/* Debug flag to print matrices in case of small matrices */
+	int dim_thread = 32;	/* Number of threads in each block */
+	
+	float pms = 0,sms=0;	/* Parallel and sequential times */
+
+	
+	if( argc != 2 ){
+		fprintf(stderr, "Atleast one argument required. Usage: %s <Side of the matrix>",argv[0]);
+		return -1;
+	}
+	
+	/* Get size of the matrix from command line */
+	size = atoi( argv[1] );
+	matrix_size = sizeof(float)* size * size;
+	
+	//if( size <= 12 ) do_print= true;
+
+	A = (float *) malloc( sizeof(float)* size * size );
+	
+	generate_notso_random_matrix( A, size );
+
+	if( do_print ){
+		printf("A=\n");
+		for( int i=0; i<size; i++ ){
+			for( int j=0; j<size; j++ ){
+				printf("%5.2f ",A[i*size + j]);
+			}
+			printf("\n");
+		}
+	}
+
+	bool is_symmetric = true;
+	bool *g_is_symmetric;
+	cudaError_t status;
+
+	/* Timers to time the parallel process */ 
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	/*********************
+	  * Start of GPU run
+	  *******************/
+	cudaEventRecord(start);
+
+	size_t freev,totalv;
+	status = cudaMalloc( &ga, sizeof(float)* size * size );
+	cudaMemGetInfo( &freev, &totalv );
+	printf("Free= %lu, total=%lu\n",freev,totalv);
+	CHECK_STATUS(status);
+
+	status = cudaMalloc( &g_is_symmetric, sizeof(bool) );
+	CHECK_STATUS(status);
+
+	status = cudaMemcpy( ga, A, matrix_size, cudaMemcpyHostToDevice );
+	CHECK_STATUS(status);
+	
+	status = cudaMemcpy( g_is_symmetric, &is_symmetric, sizeof(bool), cudaMemcpyHostToDevice );
+	CHECK_STATUS(status);
+
+	size_x = (((size-1)/2) + 1);
+	num_blocks_x = (size_x-1)/dim_thread + 1;
+	num_blocks_y = (size-2)/dim_thread + 1;
+
+	dim3 grid( dim_thread, dim_thread );
+	dim3 blocks( num_blocks_x, num_blocks_y);
+	
+	par_is_symmetric<<<blocks,grid>>> (ga, g_is_symmetric, size);
+	
+	status = cudaMemcpy( &is_symmetric, g_is_symmetric, sizeof(bool), cudaMemcpyDeviceToHost );
+	CHECK_STATUS(status);
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	/*****************
+	 * End of GPU code
+	 ****************/
+	
+	cudaEventElapsedTime( &pms, start, stop );
+
+	char *msg;
+	msg = is_symmetric ? (char *)"Symmetric":(char *)"Not Symmetric";
+	printf("The given matrix is %s\n",msg);
+	//printf("Time taken %5.7f milliseconds\n",milliseconds);
+	
+//	if( do_print ){	
+//		printf("C=\n");
+//		for( int i=0; i<size; i++ ){
+//			for( int j=0; j<size; j++ ){
+//				printf("%d ",checked[i*size + j]);
+//			}
+//			printf("\n");
+//		}
+//	}
+
+	cudaFree( ga );
+	
+	/*********************
+	 * Sequential Stuff
+	 ********************/
+	struct timespec seq_start,seq_end;
+	
+	/* clock_gettime gets the process specific time spent, as opposed to the system time expended
+	 */
+	clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &seq_start );
+	
+	seq_is_symmetric( A, &is_symmetric, size );
+
+	clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &seq_end );
+
+	/*************************
+	 * End of Sequential Stuff
+	 ************************/
+
+	msg = is_symmetric ? (char *)"Symmetric":(char *)"Not Symmetric";
+	printf("The given matrix is %s\n",msg);
+
+	/* Getting time in milliseconds for comparability */
+	sms = ( (float)seq_end.tv_sec - seq_start.tv_sec )*1000 + ( (float)seq_end.tv_nsec - seq_start.tv_nsec ) / 1000000;
+
+	printf("%12d % 12f % 12f % 12f\n",size,pms,sms,sms/pms);
+
+	free(A);
+}
+
+
+
+
+
