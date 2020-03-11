d86ddac8c36b895311ce05c5eb4962eb494981bb
Harish Ganesan <mtc1818@isical.ac.in>
2020-03-12 00:39:31 +0530

    Added Standard deviation and maximum calculation in parallel * Fixed the unequal sums. Turned out to be a silly mistake in the adding program   for parallel computation * Still facing the weird timing problem.

--- metrics.cu
+++ metrics.cu
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #ifndef __CUDA_SAFE_CALL
 cudaError_t __cuda_error;
@@ -30,9 +31,29 @@ void generate_random_vector( double *A, int size )
 		A[i] = ((double)rand())/100000;
 }
 
-__device__ void addfour( volatile double *A, int id, int threads, volatile double *B )
+__device__ void addfour( volatile double *A, int id, int threads, int size, volatile double *B )
 {
-	B[id] = A[id] + A[threads+id] + A[2*threads + id] + A[3*threads+id];
+	int index=threads+id;
+	for( int i=0; i<3; i++ ){
+		if( index < size ){
+			//printf("%d %d %d %d\n",index,id,threads,size);
+			B[id] += A[index];
+		}
+		index += threads;
+	}
+}
+
+__device__ void maxfour( volatile double *A, int id, int threads, int size, volatile double *B )
+{
+	int index=threads+id;
+	for( int i=0; i<3; i++ ){
+		if( index < size ){
+			if( B[id] < A[index] )
+				//printf("%d %d %d %d\n",index,id,threads,size);
+				B[id] = A[index];
+		}
+		index += threads;
+	}
 }
 
 __global__ void add4096( double *A, double *B, int size )
@@ -56,13 +77,94 @@ __global__ void add4096( double *A, double *B, int size )
 	__syncthreads();
 	
 	alive = alive >> 2;
+	this_block = ( this_block >= blockDim.x )? blockDim.x: this_block;
+
 	while( 1 ){
 		if( tid < alive ){
-			addfour( (double *)&nums, tid, alive, (double *)&nums );
+			addfour( (double *)&nums, tid, alive, this_block, (double *)&nums );
 			//printf(" id=%d total alive=%d tid=%d, %5.2f\n",id,alive,tid,nums[tid]);
 		}
 		if( alive == 1 )
 			break;
+		this_block = ( this_block >= alive )? alive: this_block;
+		alive = alive>>2;
+		__syncthreads();
+	}
+
+	B[id] = nums[0];
+}
+
+__global__ void addsquared4096( double *A, double *B, int size )
+{
+	__shared__ double nums[256];
+	int id = blockIdx.x;
+	int tid = threadIdx.x;
+	int offset = 4096*id;
+	int alive = blockDim.x;
+	int this_block = ( size - offset >= 4096 )? 4096: size - offset;
+	int intra_offset = 0;
+	
+	nums[tid] = 0;
+	
+	while( intra_offset < this_block ){
+		if( intra_offset + tid < this_block )
+			nums[tid] += A[ offset + intra_offset + tid ] * A[ offset + intra_offset + tid ];
+	       	intra_offset += alive;
+	}	
+
+	__syncthreads();
+	
+	alive = alive >> 2;
+	this_block = ( this_block >= blockDim.x )? blockDim.x: this_block;
+	while( 1 ){
+		if( tid < alive ){
+			addfour( (double *)&nums, tid, alive, this_block, (double *)&nums );
+			//printf(" id=%d total alive=%d tid=%d, %5.2f\n",id,alive,tid,nums[tid]);
+		}
+		if( alive == 1 )
+			break;
+		this_block = ( this_block >= alive )? alive: this_block;
+		alive = alive>>2;
+		__syncthreads();
+	}
+
+	B[id] = nums[0];
+}
+
+__global__ void max4096( double *A, double *B, int size )
+{
+	__shared__ double nums[256];
+	int id = blockIdx.x;
+	int tid = threadIdx.x;
+	int offset = 4096*id;
+	int alive = blockDim.x;
+	int this_block = ( size - offset >= 4096 )? 4096: size - offset;
+	int intra_offset = 0;
+	int tmp;
+
+	nums[tid] = -1;
+	
+	while( intra_offset < this_block ){
+		if( intra_offset + tid < this_block ){
+			tmp = A[ offset + intra_offset + tid ];
+			if( nums[tid] < tmp )
+				nums[tid] = tmp;
+		}
+	       	intra_offset += alive;
+	}	
+
+	__syncthreads();
+	
+	alive = alive >> 2;
+	this_block = ( this_block >= blockDim.x )? blockDim.x: this_block;
+	while( 1 ){
+		if( tid < alive ){
+			maxfour( (double *)&nums, tid, alive, this_block, (double *)&nums );
+			//printf(" id=%d total alive=%d tid=%d, %5.2f\n",id,alive,tid,nums[tid]);
+		}
+		if( alive == 1 )
+			break;
+		this_block = ( this_block >= alive )? alive: this_block;
 		alive = alive>>2;
 		__syncthreads();
 	}
@@ -98,6 +200,79 @@ double padd(double *A, int size)
 	return ans;
 }
 
+double psquareadd(double *A, int size )
+{
+	double *ga,*gb;
+	int vector_size = sizeof(double) * size;
+	int num_blocks = ( ((size - 1) / 4096) + 1 );
+	int out_vector = sizeof(double)* num_blocks;
+	double ans;
+
+	__CUDA_SAFE_CALL( cudaMalloc( &ga, vector_size ) );
+	__CUDA_SAFE_CALL( cudaMalloc( &gb, out_vector  ) );
+
+	__CUDA_SAFE_CALL( cudaMemcpy( ga, A, vector_size, cudaMemcpyHostToDevice ) );
+	
+	while( size > 1 ){
+		addsquared4096<<<num_blocks,256>>> (ga, gb, size);
+		size = num_blocks;
+		num_blocks = ( ((size - 1) / 4096) + 1 );
+		ga = gb;
+	}
+
+	__CUDA_SAFE_CALL( cudaMemcpy( &ans, gb, sizeof(double) , cudaMemcpyDeviceToHost ) );
+	
+	cudaFree( ga );
+	cudaFree( gb );
+
+	return ans;
+}
+
+double pmax(double *A, int size )
+{
+	double *ga,*gb;
+	int vector_size = sizeof(double) * size;
+	int num_blocks = ( ((size - 1) / 4096) + 1 );
+	int out_vector = sizeof(double)* num_blocks;
+	double ans;
+
+	__CUDA_SAFE_CALL( cudaMalloc( &ga, vector_size ) );
+	__CUDA_SAFE_CALL( cudaMalloc( &gb, out_vector  ) );
+
+	__CUDA_SAFE_CALL( cudaMemcpy( ga, A, vector_size, cudaMemcpyHostToDevice ) );
+	
+	while( size > 1 ){
+		max4096<<<num_blocks,256>>> (ga, gb, size);
+		size = num_blocks;
+		num_blocks = ( ((size - 1) / 4096) + 1 );
+		ga = gb;
+	}
+
+	__CUDA_SAFE_CALL( cudaMemcpy( &ans, gb, sizeof(double) , cudaMemcpyDeviceToHost ) );
+	
+	cudaFree( ga );
+	cudaFree( gb );
+
+	return ans;
+}
+
+
+double pmean( double *A, int size )
+{
+	double ans;
+	
+	ans = padd( A, size );
+	return ans/size;
+}
+
+double pstd( double *A, int size )
+{
+	double mean, squaredsum;
+	mean = pmean( A, size );
+	squaredsum = psquareadd( A, size )/size;
+	return sqrt(squaredsum-(mean*mean));
+}
+
 double sadd( double* A, int size )
 {
 	double ans=0;
@@ -106,6 +281,13 @@ double sadd( double* A, int size )
 		//printf("%lf \n",ans);
 	}
 	return ans;
+}
+
+double smean( double *A, int size )
+{
+	double ans;
+	ans = sadd( A, size );
+	return ans/size;
 }
 
 int main( int argc, char* argv[] )
@@ -122,7 +304,7 @@ int main( int argc, char* argv[] )
 	
 	float pms = 0,sms=0;	/* Parallel and sequential times */
 
-	double ans;
+	double mean,std,max;
 	
 	if( argc != 2 ){
 		fprintf(stderr, "Atleast one argument required. Usage: %s <Side of the matrix>",argv[0]);
@@ -161,8 +343,12 @@ int main( int argc, char* argv[] )
 	cudaEventRecord(stop);
 	
 	
-	ans = 0;
-	ans = padd( A, size );
+	mean = 0;
+	std = 0;
+	max = 0;
+	mean = pmean( A, size );
+	std = pstd( A, size );
+	max = pmax( A, size );
 
 	cudaEventSynchronize(stop);
 	/*****************
@@ -171,8 +357,9 @@ int main( int argc, char* argv[] )
 	
 	cudaEventElapsedTime( &pms, start, stop );
 
-	printf(" Total sum is %lf\n",ans);
-
+	printf("Mean is %lf\n",mean);
+	printf("Std is %lf\n",std);
+	printf("Max is %lf\n",max);
 	/*********************
 	 * Sequential Stuff
 	 ********************/
@@ -182,7 +369,7 @@ int main( int argc, char* argv[] )
 	 */
 	clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &seq_start );
 	
-	ans = sadd( A, size );	
+	mean = smean( A, size );	
 
 	clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &seq_end );
 
@@ -190,13 +377,19 @@ int main( int argc, char* argv[] )
 	 * End of Sequential Stuff
 	 ************************/
 
-	printf("Sum is %lf\n",ans);
+	printf("Mean is %lf\n",mean);
 	
 	/* Getting time in milliseconds for comparability */
 	sms = ( (float)seq_end.tv_sec - seq_start.tv_sec )*1000 + ( (float)seq_end.tv_nsec - seq_start.tv_nsec ) / 1000000;
 	printf("%12s %12s %12s %12s\n","N","Parallel","Sequential","Speedup");
 	printf("%12d % 12f % 12f % 12f\n",size,pms,sms,sms/pms);
-
+	/*
+	printf("<html>\n\t<body>\n\t\t<table>\n");
+	printf("<tr>\n");
+	printf("\t<td> %12d </td>\n\t<td>% 12f</td>\n\t<td>% 12f</td>\n\t<td>% 12f</td>\n",size,pms,sms,sms/pms);
+	printf("</tr>\n");
+	printf("</table>\n</body>\n</html>\n");
+	*/
 	free(A);
 }
 
